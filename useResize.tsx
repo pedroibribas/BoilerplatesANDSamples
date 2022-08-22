77b36488614fbe1d516aa423b67220e6b82ceb4a
Pedro Ribas <88813381+pedroibribas@users.noreply.github.com>
2022-08-22 18:15:40 -0400

    Update useResize.tsx

--- useResize.tsx
+++ useResize.tsx
@@ -1,49 +1,53 @@
 import { useLayoutEffect, useState } from "react";
 
 interface IDimensions {
-  width: number;
-  height: number;
+  width: undefined | number;
+  height: undefined | number;
 };
 
 export const useResize = (el: HTMLElement) => {
-  // Inicializar estado para as dimensões width x height
+  // Inicializar estado das dimensões
   const [dimensions, setDimensions] = useState<IDimensions>({
-    width: 0,
-    height: 0
+    width: undefined, // client-side e server-side match (no server, window é undefined)
+    height: undefined,
   });
 
   // Monitorar comportamento do elemento
   useLayoutEffect(() => {
-    const handleResize = () => {
-      setDimensions(prevState => ({
-        ...prevState,
-        width: el.offsetWidth,
-        height: el.offsetWidth
-      }));
-    };
+    // Prevenir 'document is not defined'
+    if (typeof window !== 'undefined') {
+      const handleResize = () => {
+        setDimensions(prevState => ({
+          ...prevState,
+          width: el.offsetWidth,
+          height: el.offsetWidth
+        }));
+      };
 
-    // Obter dimensões no carregamento da página
-    if (el) {
-      handleResize();
-    };
+      // Obter dimensões no carregamento da página
+      if (el) {
+        handleResize();
+      };
 
-    // Obter dimensões no redimensionamento
-    window.addEventListener('resize', handleResize);
+      // Obter dimensões no redimensionamento
+      window.addEventListener('resize', handleResize);
 
-    // Remover listener para evitar bugs
-    return () => {
-      window.removeEventListener('resize', handleResize);
+      // Remover listener para evitar bugs
+      return () => {
+        window.removeEventListener('resize', handleResize);
+      };
     };
   }, [el]);
 
-  // Retornar dimensões width x height
+  // Retornar dimensões
   return dimensions;
 };
 
 /**
  * Resumo
- * Inicializar estado para as dimensões width x height
+ * Inicializar estado para as dimensões
  * Monitorar comportamento do elemento
+ * Prevenir 'document is not defined'
  * Obter dimensões no carregamento da página
  * Obter dimensões no redimensionamento
  * Remover listener para evitar bugs
@@ -52,5 +56,7 @@ export const useResize = (el: HTMLElement) => {
  * Sources
  * https://linguinecode.com/post/how-to-use-react-useref-with-typescript
  * https://linguinecode.com/post/when-to-use-useref-and-uselayouteffect
+ * https://stackoverflow.com/questions/63406435/how-to-detect-window-size-in-next-js-ssr-using-react-hook
+ * https://www.joshwcomeau.com/react/the-perils-of-rehydration/
  */
 
